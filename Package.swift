bc369b7e3917933bf11bb990697b5f4007daea0f
Todd Bohman <toddbohm@microsoft.com>
2023-10-10 14:18:49 -0600

    adding initial package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MicrosoftIntuneTunnelSDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MicrosoftIntuneTunnelSDK",
+            targets: ["MicrosoftIntuneTunnelSDK"]),
+    ],
+    targets: [
+        .target(name: "MicrosoftIntuneTunnelSDK", dependencies: ["MicrosoftTunnelApi"]),
+        .binaryTarget(name: "ssl", path: "ssl.xcframework"),
+        .binaryTarget(name: "crypto", path: "crypto.xcframework"),
+        .binaryTarget(name: "MCPCommon", path: "MCPCommon.xcframework"),
+        .binaryTarget(name: "MCPCore", path: "MCPCore.xcframework"),
+        .binaryTarget(name: "MCPPluginUnencryptedFile", path: "MCPPluginUnencryptedFile.xcframework"),
+        .binaryTarget(name: "MSTAPNextPluginSecurityOpenssl", path: "MSTAPNextPluginSecurityOpenssl.xcframework"),
+        .binaryTarget(name: "MSTAPNextPluginVpnMicrosoftTunnel", path: "MSTAPNextPluginVpnMicrosoftTunnel.xcframework"),
+        .binaryTarget(name: "MicrosoftTunnelApi", path: "MicrosoftTunnelApi.xcframework"),
+    ]
+)
---
+++ Sources/MicrosoftIntuneTunnelSDK/MicrosoftIntuneTunnelSDK.swift
@@ -0,0 +1,72 @@
+import Foundation
+import MicrosoftTunnelApi
+
+open class MicrosoftTunnelDelegate : NSObject, MicrosoftTunnelApi.MicrosoftTunnelDelegate {
+   
+    static var instance: MicrosoftTunnelDelegate = MicrosoftTunnelDelegate()
+    
+    private let config: NSMutableDictionary = NSMutableDictionary.init()
+    private let api: MicrosoftTunnel = MicrosoftTunnel.sharedInstance
+    
+    private override init(){
+        super.init()
+    }
+    
+    public func launch(){
+        if !api.launchEnrollment()  {
+            let error = api.microsoftTunnelInitialize(with: self, logDelegate: LogDelegate(), config: (self.config as! [String : String]))
+            if error != NoError {
+                NSLog("Failed to initialize MicrosoftTunnel API!")
+            }
+        }
+    }
+    
+    public func signout(){
+        if !api.launchUnenrollment(true)  {
+            let error = api.microsoftTunnelInitialize(with: self, logDelegate: LogDelegate(), config: (self.config as! [String : String]))
+            if error != NoError {
+                NSLog("Failed to initialize MicrosoftTunnel API!")
+            }
+        }
+    }
+    
+    public func handleMSALResponse(_ response: URL, _ sourceApplication: String) -> Bool {
+        return api.handleMSALResponse(response, sourceApplication: sourceApplication)
+    }
+    
+    // MARK: MicrosoftTunnelDelegate Protocol Stubs
+    
+    open func onReceivedEvent(_ event: MicrosoftTunnelStatus) {
+        
+    }
+    
+    open func onInitialized() {
+        
+    }
+    
+    open func onConnected() {
+        
+    }
+    
+    open func onReconnecting() {
+        
+    }
+    
+    open func onUserInteractionRequired() {
+        
+    }
+    
+    open func onDisconnected() {
+        
+    }
+    
+    open func onError(_ error: MicrosoftTunnelError) {
+        
+    }
+}
+
+public class LogDelegate: NSObject, MicrosoftTunnelApi.MicrosoftTunnelLogDelegate {
+    public func logMessage(_ level: UInt32, logClass: UInt32, pTime: UnsafePointer<CChar>, pLevel: UnsafePointer<CChar>, pClassLabel: UnsafePointer<CChar>, pLog: UnsafePointer<CChar>) {
+        NSLog("%s [%s] [%d] %s", pLevel, pClassLabel, pthread_mach_thread_np(pthread_self()), pLog)
+    }
+}
